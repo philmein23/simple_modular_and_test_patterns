d4a2c9389160209a4cc68d91f63761d563cf116d
Phil Nguyen <philnguyen@Phils-MacBook-Pro.local>
2016-04-06 10:47:12 -0700

    modified code to match style and code requirements

--- PhillipNguyen/lib/phil_greeting.js
+++ PhillipNguyen/lib/phil_greeting.js
@@ -1,7 +1,3 @@
-const Phil = module.exports = function(name) {
-  this.name = name;
-};
-
-Phil.prototype.greet = function() {
-  return "Hello " + this.name;
+module.exports = function(name) {
+  return 'Hello ' + (name || "world");
 };
--- PhillipNguyen/test/phil_greeting_test.js
+++ PhillipNguyen/test/phil_greeting_test.js
@@ -1,11 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Phil = require(__dirname + '/../lib/phil_greeting');
-
-const phil = new Phil("Phil");
+const phil = require(__dirname + '/../lib/phil_greeting');
 
 describe('phil', () => {
   it('should return a greeting string',  () => {
-    expect(phil.greet("Phil")).to.eql('Hello Phil');
+    expect(phil('Phil')).to.eql('Hello Phil');
   });
 });
